956141d99bb98fd7aecd3b4f9969746b8d1eace4
Mozammel Hoshen Chowdhury <mozammel990@gmail.com>
2022-08-28 22:36:53 +0600

    product details page updated

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,15 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
 class Products {
-  Products({
-    required this.title,
-    required this.description,
-    required this.price,
-    required this.imageUrl,
-  });
+  Products(
+      {required this.title,
+      required this.description,
+      required this.price,
+      required this.imageUrl,
+      required this.size,
+      required this.color,
+      required this.fullDecription});
 
   final String title;
   final String description;
   final String price;
   final String imageUrl;
+  final String size;
+  final Color color;
+  final String fullDecription;
 
   static List<Products> getProducts() {
     return <Products>[
@@ -17,37 +24,65 @@ class Products {
           title: "BeoPlay Speaker",
           description: "Bang and Olufsen",
           price: '\$200',
-          imageUrl: "assets/images/products/speaker.png"),
+          imageUrl: "assets/images/products/speaker.png",
+          size: "M",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
       Products(
           title: "Leather Wristwatch",
           description: "Tag Heuer",
           price: '\$400',
-          imageUrl: "assets/images/products/watch.png"),
+          imageUrl: "assets/images/products/watch.png",
+          size: "M",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
       Products(
           title: "B&o Desk Lamp",
           description: "Bang and Olufsen",
           price: '\$450',
-          imageUrl: "assets/images/products/lamp.png"),
+          imageUrl: "assets/images/products/lamp.png",
+          size: "M",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
       Products(
           title: "BeoPlay Stand Speaker",
           description: "Bang and Olufsen",
           price: '\$300',
-          imageUrl: "assets/images/products/speaker1.png"),
+          imageUrl: "assets/images/products/speaker1.png",
+          size: "M",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
       Products(
           title: "Leather Wristwatch",
           description: "Tag Heuer",
           price: '\$400',
-          imageUrl: "assets/images/products/watch.png"),
+          imageUrl: "assets/images/products/watch.png",
+          size: "M",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
       Products(
           title: "Leather Wristwatch",
           description: "Tag Heuer",
           price: '\$400',
-          imageUrl: "assets/images/products/watch.png"),
+          imageUrl: "assets/images/products/watch.png",
+          size: "M",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
       Products(
           title: "Nike Dri-FIT Long Sleeve",
           description: "Long Sleeve",
           price: '\$200',
-          imageUrl: "assets/images/products/nike.png"),
+          imageUrl: "assets/images/products/nike.png",
+          size: "XL",
+          color: const Color(0xFF3D82AE),
+          fullDecription:
+              "Nike Dri-FIT is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer."),
     ];
   }
 }
--- lib/screens/products/productsdetails.dart
+++ lib/screens/products/productsdetails.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerce/models/products.dart';
 import 'package:ecommerce/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetails extends StatelessWidget {
   final Products product;
@@ -9,68 +11,282 @@ class ProductDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 53.0),
-            child: Stack(
-              children: [
-                SizedBox(
-                  height: 500,
-                  child: Image.asset(
-                    product.imageUrl,
-                    fit: BoxFit.fitHeight,
+      bottomNavigationBar: SizedBox(
+        height: 84,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                children: [
+                  const Text(
+                    "Price",
+                    style: TextStyle(
+                        color: Color(0xFF929292),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
                   ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    product.price,
+                    style: const TextStyle(
+                        color: ePrimaryColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(146, 50),
+                  primary: const Color(0xFF00C569),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0, top: 20),
-                  child: IconButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
+                child: const Text(
+                  ' ADD',
+                  style: TextStyle(fontSize: 17),
+                ))
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 53.0),
+              child: Stack(
+                children: [
+                  SizedBox(
+                    height: 500,
+                    width: 400,
+                    child: Image.asset(
+                      product.imageUrl,
+                      fit: BoxFit.fill,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 320.0, top: 20),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: ePrimaryColor,
-                          width: 2,
-                        ),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(50))),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, top: 20),
                     child: IconButton(
-                      onPressed: () {},
+                      onPressed: () => Navigator.of(context).pop(),
                       icon: const Icon(
-                        Icons.star_border_rounded,
+                        Icons.arrow_back_ios,
                         color: Colors.black,
-                        size: 30,
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 320.0, top: 20),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset(
+                        'assets/icons/wishlist.png',
+                        height: 70,
+                        width: 70,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 12.0),
-            child: Row(
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Row(
+                children: [
+                  Text(
+                    product.title,
+                    style: const TextStyle(
+                        fontSize: 26, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(
-                  product.title,
-                  style: const TextStyle(
-                      fontSize: 26, fontWeight: FontWeight.bold),
+                Container(
+                  height: 40,
+                  width: 160,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color(0XFFEBEBEB),
+                      width: 2,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(50)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        "Size",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        product.size,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  width: 160,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color(0XFFEBEBEB),
+                      width: 2,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(50)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        "Color",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                            color: product.color, shape: BoxShape.circle),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                children: const [
+                  Text(
+                    "Details",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ReadMoreText(
+                trimLines: 2,
+                colorClickableText: ePrimaryColor,
+                trimMode: TrimMode.Line,
+                trimCollapsedText: 'Show more',
+                trimExpandedText: 'Show less',
+                product.fullDecription,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                children: const [
+                  Text(
+                    "Reviews",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(100.0),
+                      child: Image.asset(
+                        "assets/images/persons/person1.png",
+                        height: 70,
+                        width: 70,
+                      ),
+                    ),
+                    const Text(
+                      "Samuel Smith",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      itemSize: 20,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(100.0),
+                      child: Image.asset(
+                        "assets/images/persons/person2.png",
+                        height: 70,
+                        width: 70,
+                      ),
+                    ),
+                    const Text(
+                      "Beth Aida",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      itemSize: 20,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
